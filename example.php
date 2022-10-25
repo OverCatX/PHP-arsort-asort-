36760084c21a32db85268b2509b3e2a7c650ec9e
OverCatX <74818019+OverCatX@users.noreply.github.com>
2022-10-25 23:43:29 +0700

    Create example.php

---
+++ example.php
@@ -0,0 +1,31 @@
+/* Func arsort เรัยงจากค่ามากไปหาน้อย */
+$scores = array("OverCatX" => 90,
+                "RictorX" => 80,
+                "TheBesses" => 70);
+arsort($scores);
+foreach($scores as $key => $values){
+   echo "Name: ".$key." Score: ".$values."\n";
+}
+
+/* Func asort เรียงจากค่าน้อยไปหามาก */
+$scores = array("OverCatX" => 90,
+                "RictorX" => 80,
+                "TheBesses" => 70);
+asort($scores);
+foreach($scores as $key => $values){
+   echo "Name: ".$key." Score: ".$values."\n";
+}
+
+/* Exanple ทำ TopScore เรียงจากคะแนนที่มากสุดไปหาน้อยสุด*/
+$scores = array("OverCatX" => 90,
+                "RictorX" => 80,
+                "TheBesses" => 70);
+arsort($scores);
+$i=0;
+foreach($scores as $key => $values){
+   echo "อันดับที่ ".$i." ".$key." Score: ".$values."\n";
+   if($i > count($scores)){
+   	break;
+   }
+   ++$i;
+}
